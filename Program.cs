66248d95ff25f398488c7b45af5a008cc13668ea
Little-Pink-Dragon <111520046+Little-Pink-Dragon@users.noreply.github.com>
2022-09-08 03:05:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 1;
+bool not = true;
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + n);
+while (a <= n)
+{
+    if (a % 2 != 1)
+    {
+        Console.Write(a + ", ");
+        not = false;
+    }
+    a = a + 1;
+}
+
+    if (not)
+    {
+    Console.WriteLine("Нет чётных чисел!");
+    }
